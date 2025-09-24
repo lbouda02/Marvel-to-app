e00e9c640ad2ce35d5f441d791f63a1ba6ca8fc0
lbouda02 <lucas.boudaud@etu.univ-poitiers.fr>
2025-09-24 18:01:09 +0200

    Use dynamic list for characters

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
+import characters from './data/characters.json'
 
 function App() {
   return (
     <>
       <h1>Marvel Characters</h1>
       <ul>
-        <li>Beast</li>
-        <li>Captain America</li>
-        <li>Deadpool</li>
+        {characters.map(character => (
+          <li key={character.id}>{character.name}</li>
+        ))}
       </ul>
     </>
   )
