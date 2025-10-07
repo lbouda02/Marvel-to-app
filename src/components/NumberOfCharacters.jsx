556aac443cf90bcb3ac047ace253f538e51e553a
lbouda02 <lucas.boudaud@etu.univ-poitiers.fr>
2025-10-07 18:21:59 +0200

    test(NumberOfCharacters): 🚨 add unit tests for no character, singular and plural cases

--- src/components/NumberOfCharacters.jsx
+++ src/components/NumberOfCharacters.jsx
@@ -1,7 +1,20 @@
+// src/components/NumberOfCharacters.jsx
+
+/**
+ * NumberOfCharacters component
+ * @param {*} characters - Array of characters, default is an empty array
+ * @returns JSX element displaying the number of characters
+ */
 export default function NumberOfCharacters({ characters = [] }) {
-    if (characters.length === 0) {
-      return <p>There is no character</p>;
+    const count = characters.length;
+
+    if (count === 0) {
+        return <p>There is no character</p>;
     }
 
-    return <p>There is {characters.length} characters</p>;
+    if (count === 1) {
+        return <p>There is 1 character</p>;
+    }
+
+    return <p>There are {count} characters</p>;
 }
